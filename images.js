9a1e9006e911faca9cf4eb3631b3df321d72b840
scholtzk <112542269+scholtzk@users.noreply.github.com>
2022-12-22 21:00:07 +1300

    Add files via upload

---
+++ images.js
@@ -0,0 +1,17 @@
+import imageOne from '../images/1.jpg';
+import imageTwo from '../images/2.jpg';
+import imageThree from '../images/3.jpg';
+import imageFour from '../images/4.jpg';
+
+
+const images = {
+    imageOne,
+    imageTwo,
+    imageThree,
+    imageFour
+}
+
+
+
+
+export default images;
